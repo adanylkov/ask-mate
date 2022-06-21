5a779c551ef25155b59d105cbac424a54d397046
Jacek Walaszek <w.jacek99@gmail.com>
2022-06-21 12:09:22 +0200

    ask a question template

--- connection.py
+++ connection.py
@@ -6,23 +6,22 @@ QUESTION_HEADER = ["id","submission_time","view_number","vote_number","title","m
 
 
 def read_data_from_file(file_name):
-    with open(f"data/{file_name}", 'r') as file:
-            reader = csv.DictReader(file)
-            stories = []
-            for story in reader:
-                stories.append(story)
-            return stories
+    with open(f"sample_data/{file_name}", 'r') as file:
+        reader = csv.DictReader(file)
+        stories = []
+        for story in reader:
+            stories.append(story)
+        return stories
 
 
 def write_data_to_file(file_name, data, data_header):
-    with open(f"data/{file_name}", 'w') as file:
+    with open(f"sample_data/{file_name}", 'w') as file:
         writer = csv.DictWriter(file, data_header)
         writer.writeheader()
         writer.writerows(data)
 
 
-
 def add_data_to_file(file_name, data, data_header):
-    with open(f"data/{file_name}", 'a') as file:
+    with open(f"sample_data/{file_name}", 'a+') as file:
         writer = csv.DictWriter(file, data_header)
         writer.writerow(data)
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from connection import add_data_to_file
+import connection
 
 app = Flask(__name__)
 
@@ -8,5 +10,21 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/add-question", methods=["GET","POST"])
+def ask_question():
+    if request.method == "POST":
+        title = request.form.get("title")
+        message = request.form.get("message")
+        my_list = {'id': 81,'submission_time': 123123,'view_number':1,'vote_number':2,'title':title, 'message':message, 'image':'None'}
+        add_data_to_file("question.csv", my_list, connection.QUESTION_HEADER)
+        return render_template("add-question.html")
+    # return render_template("add-question.html")
+    # id,submission_time,view_number,vote_number,title,message,image
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+    )
