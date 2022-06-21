84ce3924d5d9420f2fb2547e83aa78857256aed4
Jacek Walaszek <w.jacek99@gmail.com>
2022-06-21 12:35:24 +0200

    commit before merge

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ QUESTION_HEADER = ["id","submission_time","view_number","vote_number","title","m
 
 
 def read_data_from_file(file_name):
-    with open(f"sample_data/{file_name}", 'r') as file:
+    with open(f"data/{file_name}", 'r') as file:
         reader = csv.DictReader(file)
         stories = []
         for story in reader:
@@ -15,14 +15,14 @@ def read_data_from_file(file_name):
 
 
 def write_data_to_file(file_name, data, data_header):
-    with open(f"sample_data/{file_name}", 'w') as file:
+    with open(f"data/{file_name}", 'w') as file:
         writer = csv.DictWriter(file, data_header)
         writer.writeheader()
         writer.writerows(data)
 
 
 def add_data_to_file(file_name, data, data_header):
-    with open(f"sample_data/{file_name}", 'a+') as file:
+    with open(f"data/{file_name}", 'a+') as file:
         writer = csv.DictWriter(file, data_header)
         writer.writerow(data)
         
