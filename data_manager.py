4b6b61d1a2486ed147fd309df94ad2034cd99183
Andrii Danylkov <danilkovxp@gmail.com>
2022-06-22 09:42:32 +0200

    Revert "del question"
    This reverts commit d0cbe3730f425058647816a90a399920de17497b.

--- data_manager.py
+++ data_manager.py
@@ -6,38 +6,23 @@ def get_question_by_id(id):
     questions = connection.read_data_from_file('question.csv')
     return list(filter(lambda question: question['id'] == str(id), questions)).pop()
 
-
 def answers_by_question_id(question_id):
     answers = connection.read_data_from_file('answer.csv')
     return list(filter(lambda answer: answer['question_id'] == str(question_id), answers))
 
-
 def questions():
     questions = connection.read_data_from_file('question.csv')
     return questions
-
 
 def add_question(title, message):
     question = {"id": util.create_id(), "submission_time": util.make_timestamp(), "view_number": 0, "vote_number": 0, "title": title, "message": message, "image": None}
     connection.add_data_to_file("question.csv", question, connection.QUESTION_HEADER)
     return question["id"]
 
-
 def add_answer(question_id, message):
     #id,submission_time,vote_number,question_id,message,image
     answer = {"id": util.create_id(is_question=False), "submission_time": util.make_timestamp(), "vote_number": 0, "question_id": question_id, "message": message, "image": None}
     connection.add_data_to_file('answer.csv', data=answer, data_header=connection.ANSWER_HEADER)
-
-
-def del_question(question_id):
-    all_questions = questions()
-    print(question_id)
-    print(all_questions[question_id-1])
-    del all_questions[question_id-1]
-
-    print(all_questions)
-    connection.write_data_to_file("question.csv", all_questions, data_header=connection.QUESTION_HEADER)
-
 
 if __name__ == "__main__":
     print(questions()[0])
